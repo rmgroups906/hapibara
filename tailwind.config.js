dcf0b3ed69f062b4cc4d2bb1413c796223701273
rmgroups906 <rmgroups24@gmail.com>
2025-07-04 14:30:39 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      theme: {
+  extend: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+  },
+}
+
+    },
+  },
+  plugins: [],
+}
